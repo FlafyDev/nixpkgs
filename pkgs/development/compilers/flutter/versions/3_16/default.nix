12eeaed73d56802adae5365a17dc79ce8c694cb5
FlafyDev <flafyarazi@gmail.com>
2024-01-11 01:06:46 +0200

    flutter: 3.13.8 -> 3.16.6

---
+++ pkgs/development/compilers/flutter/versions/3_16/default.nix
@@ -0,0 +1,54 @@
+{
+  version = "3.16.6";
+  engineVersion = "3f3e560236539b7e2702f5ac790b2a4691b32d49";
+  dartVersion = "3.2.3";
+  dartHash = {
+    x86_64-linux = "sha256-mPR7HS8GdJD37FmBPYoFPUo+jHlSVz2epzYu1dX4WmA=";
+    aarch64-linux = "sha256-2Qa+VVbV5vdcuKn75+2Xkk9vQ0ycBVv+mRjSnMJTB8o=";
+    x86_64-darwin = "sha256-KU1/2wLOIIsb6K4wFy4/4sxTc0oYVSgZWa4Mi01cr/Y=";
+    aarch64-darwin = "sha256-Y2lBBAATz46PI+bJ3LUSOLQA7LVITNAd/Ti+jz048cw=";
+  };
+  flutterHash = "sha256-8XAZ/dsCX6p4WNOdedEGzH6RZCLLiqfeYOs7TjD8qHA=";
+  artifactHashes = {
+    android = {
+      aarch64-linux = "sha256-kg5nsPlgVlaVO9C7Ve38MUyTyWSmCanWSE1Y+yyJtqE=";
+      x86_64-darwin = "sha256-kg5nsPlgVlaVO9C7Ve38MUyTyWSmCanWSE1Y+yyJtqE=";
+      x86_64-linux = "sha256-kg5nsPlgVlaVO9C7Ve38MUyTyWSmCanWSE1Y+yyJtqE=";
+    };
+    fuchsia = {
+      aarch64-linux = "sha256-eu0BERdz53CkSexbpu3KA7O6Q4g0s9SGD3t1Snsk3Fk=";
+      x86_64-darwin = "sha256-eu0BERdz53CkSexbpu3KA7O6Q4g0s9SGD3t1Snsk3Fk=";
+      x86_64-linux = "sha256-eu0BERdz53CkSexbpu3KA7O6Q4g0s9SGD3t1Snsk3Fk=";
+    };
+    ios = {
+      aarch64-linux = "sha256-wg2Ri7FAhv3wrzMuYTDzkLO5nbIPy0H9tI8TI9z6Fhw=";
+      x86_64-darwin = "sha256-wg2Ri7FAhv3wrzMuYTDzkLO5nbIPy0H9tI8TI9z6Fhw=";
+      x86_64-linux = "sha256-wg2Ri7FAhv3wrzMuYTDzkLO5nbIPy0H9tI8TI9z6Fhw=";
+    };
+    linux = {
+      aarch64-linux = "sha256-QsTTNye/pjqK8jNSe20t6xY//EeUYxnGguWjaCaUm9A=";
+      x86_64-darwin = "sha256-osmqqm9y261qTQEg4ponQlqub5PtEOaJ1npaL0ZDEog=";
+      x86_64-linux = "sha256-osmqqm9y261qTQEg4ponQlqub5PtEOaJ1npaL0ZDEog=";
+    };
+    macos = {
+      aarch64-linux = "sha256-54Fvmq7Ee8aWXlh0e1ukaxMwMMrEESmsra2b8JOQDec=";
+      x86_64-darwin = "sha256-54Fvmq7Ee8aWXlh0e1ukaxMwMMrEESmsra2b8JOQDec=";
+      x86_64-linux = "sha256-54Fvmq7Ee8aWXlh0e1ukaxMwMMrEESmsra2b8JOQDec=";
+    };
+    universal = {
+      aarch64-linux = "sha256-yHRVZelR/y30DvAKQz8/IHbiGsvq1tfzsNbx2VMn2YU=";
+      x86_64-darwin = "sha256-w+nQUeit0CJejQKPY7/ZwpKWvb2owewH+FcTy+wdGY8=";
+      x86_64-linux = "sha256-w+nQUeit0CJejQKPY7/ZwpKWvb2owewH+FcTy+wdGY8=";
+    };
+    web = {
+      aarch64-linux = "sha256-TOmro8ugInKec/mRhpAs8FYonmHNKFZmX/xZYFyslI0=";
+      x86_64-darwin = "sha256-TOmro8ugInKec/mRhpAs8FYonmHNKFZmX/xZYFyslI0=";
+      x86_64-linux = "sha256-TOmro8ugInKec/mRhpAs8FYonmHNKFZmX/xZYFyslI0=";
+    };
+    windows = {
+      aarch64-linux = "sha256-qPN3TQnKOBk+y4Z+3v4Zrto6s6wj75WYbsoZ4C3KOb0=";
+      x86_64-darwin = "sha256-qPN3TQnKOBk+y4Z+3v4Zrto6s6wj75WYbsoZ4C3KOb0=";
+      x86_64-linux = "sha256-qPN3TQnKOBk+y4Z+3v4Zrto6s6wj75WYbsoZ4C3KOb0=";
+    };
+  };
+}
